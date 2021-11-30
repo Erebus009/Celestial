ff69867c32f475520e7795a020726da1e121833a
Travis Puryear <travispuryear678@gmail.com>
2021-11-30 17:24:30 -0500

    Corrected Folder Structure.

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+
+
+const Header = () =>{
+    return <div><h1>Header</h1></div>
+}
+
+export default Header;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+    type Query {
+        users: [User]
+        user(username: String!): User
+        
+      }
+
+    
+`;
+
+
+
+
+
+
+
+module.exports = typeDefs;
