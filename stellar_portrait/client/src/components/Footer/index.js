2646bdef878fb085bb6e7630a7036867b4610c8e
Travis Puryear <travispuryear678@gmail.com>
2021-11-30 17:28:28 -0500

    Removed extra fodler.

--- stellar_portrait/client/src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
-
-
-const Footer = () =>{
-    return <div><h1>Footer</h1></div>
-}
-
-export default Footer;
--- stellar_portrait/client/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
-
-
-const Header = () =>{
-    return <div><h1>Header</h1></div>
-}
-
-export default Header;
