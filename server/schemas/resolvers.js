71fe5719350e44fca3fb52a815f53b42b649a031
Travis Puryear <travispuryear678@gmail.com>
2021-11-30 22:49:09 -0500

    Models and Schemas setup for query and mutations of posts/users.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,41 +1,58 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User,Post } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find();
+      return User.find().populate('posts');
     },
     user: async (parent, { username }) => {
       return User.findOne({ username });
     },
+    posts: async ( parent, {username}) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({createdAt: -1});
+    },
+    post: async (parent,{postId}) => { 
+      return Post.findOne({_id: postId})
+
+    }
   },
 //  User AUTH mutation
-//
-//   Mutation: {
-//     addUser: async (parent, { username, email, password }) => {
-//       const user = await User.create({ username, email, password });
-//       const token = signToken(user);
-//       return { token, user };
-//     },
-//     login: async (parent, { email, password }) => {
-//       const user = await User.findOne({ email });
 
-//       if (!user) {
-//         throw new AuthenticationError("No user found with this email address");
-//       }
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-//       const correctPw = await user.isCorrectPassword(password);
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
 
-//       if (!correctPw) {
-//         throw new AuthenticationError("Incorrect credentials");
-//       }
+      const correctPw = await user.isCorrectPassword(password);
 
-//       const token = signToken(user);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-//       return { token, user };
-//     },
-//   },
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addPost: async(parent,{text,postAuthor}) => {
+      const post = await Post.create({ text, postAuthor });
+      
+      await User.findOneAndUpdate(
+        {username: postAuthor},
+        {$addToSet:{posts: post._id}}
+      )
+      return post
+    }
+  },
 };
 module.exports = resolvers;
