992c583db926b40b5726740a67f3e84d3db98e2e
MoeFar95 <89990024+MoeFar95@users.noreply.github.com>
2021-12-03 11:04:57 -0500

    working comment

---
+++ client/src/components/Comments/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Comments = () => {
+    return (
+      <comment className="w-100 mt-auto bg-secondary p-4">
+        <div className="container text-center mb-5">
+          <h4>
+            Made with{' '}
+            <span
+              className="emoji"
+              role="img"
+              aria-label="heart"
+              aria-hidden="false"
+            >
+              ❤️
+            </span>{' '}
+            by the Tech Thoughts team.
+          </h4>
+        </div>
+      </comment>
+    );
+  };
+
+export default Comments;
