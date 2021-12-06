210782739dfaca3001e9e4aeb225b3c40a029070
MoeFar95 <89990024+MoeFar95@users.noreply.github.com>
2021-12-06 13:04:33 -0500

    Everything So Far

---
+++ client/src/components/Profile/AddProfile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const AddProfile = () => {
+    return (
+      <AddProfile className="w-100 mt-auto bg-secondary p-4">
+        <div className="container text-center mb-5">
+          <h4>
+            Made with{' '}
+            <span
+              className="emoji"
+              role="img"
+              aria-label="heart"
+              aria-hidden="false"
+            >
+              ❤️
+            </span>{' '}
+            by the Tech Thoughts team.
+          </h4>
+        </div>
+      </AddProfile>
+    );
+  };
+
+export default AddProfile;
